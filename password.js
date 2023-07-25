f4735f3557824596de36e7aa7eb3b05283c81498
Abdul ahad.S <139561652+AHADBAVA@users.noreply.github.com>
2023-07-25 22:25:19 +0530

    Update password.js

--- password.js
+++ password.js
@@ -2,7 +2,7 @@ function subbtn() {
     var email = document.getElementById('email').value;
     var password = document.getElementById('password1').value;
 
-    if (email === "baba@gmail.com" && password === "baba@10") {
+    if (email === "abdulahad123@gmail.com" && password === "ahad123@10") {
         window.location.href = "welcome.html";
     } else {
         document.getElementById('display').innerHTML = "Email or password is incorrect";
